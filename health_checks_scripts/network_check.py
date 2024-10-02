711640fe52db2f196af323bb72d67bd479f5eb18
Emil Parvanov <100536596+parvanoe@users.noreply.github.com>
2024-10-02 13:51:58 +0300

    Update network_check.py

--- health_checks_scripts/network_check.py
+++ health_checks_scripts/network_check.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python3
 
+import requests
+import socket
+
+
+def check_localhost():
+    localhost = socket.gethostbyname('localhost')
+    return localhost == "127.0.0.1"
+
+
+def check_connectivity():
+    request = requests.get("http://www.google.com")
+    response = request
+    return response.status_code == 200
+
+if check_localhost() and check_connectivity():
+    print("Everything ok")
+else:
+    print("Network checks failed")
